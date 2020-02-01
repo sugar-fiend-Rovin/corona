4d774350d5cd0f8cdb3d0bbb534ce00d42b8ba69
Unknown <darrenyu_dy@hotmail.com>
2020-01-31 22:28:54 -0800

    updated scraper

--- scraper.py
+++ scraper.py
@@ -1,1 +1,10 @@
-print("hello")
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.cnn.com/us/live-news/kobe-bryant-dies-in-helicopter-crash/index.html")
+coverpage = r.content
+
+soup = BeautifulSoup(coverpage, 'html5lib')
+
+coverpage_news = soup.find_all('h')
+coverpage_news[4].get_text()
