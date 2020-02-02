5f82e306b7b6e0f7d80c61e7069d56ad65b2efea
Zachary Sun <zbsun@uci.edu>
2020-02-01 17:07:24 -0800

    g

--- map.py
+++ map.py
@@ -1,3 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+#data_folder = Path("Documents/corona")
+df = pd.read_scv('Documents/corona/coordinates.csv')
+pd.head()
+
+BBox = ((df.longitude.min(),df.longitude.max(), df.latitude.min(), df.latitude.max())
+
