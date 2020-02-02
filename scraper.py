fcda60d6b7c1d16e6e1bcdfbe14acb817771bf29
Unknown <darrenyu_dy@hotmail.com>
2020-02-01 18:18:20 -0800

    new scraper

--- scraper.py
+++ scraper.py
@@ -1,10 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+from pathlib import Path
+from newspaper import Article
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-r = requests.get("https://www.cnn.com/us/live-news/kobe-bryant-dies-in-helicopter-crash/index.html")
+article = Article("https://abc7news.com/5895060/")
+article.download()
+article.parse()
+print(article.text)
+
+r = requests.get("https://abc7news.com/5895060/")
 coverpage = r.content
-
+s = ""
 soup = BeautifulSoup(coverpage, 'html5lib')
-
-coverpage_news = soup.find_all('h')
+for i in soup.find_all("html"):
+	s += (i.text + " ")
+print(s)
+coverpage_news = soup.find_all('coronavirus')
+print(coverpage_news)
 coverpage_news[4].get_text()
